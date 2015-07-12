110c655c3ccc5c9c76424ce87691517a54e987e0
Anurag Koul <koulanurag@gmail.com>
2015-07-12 16:37:03 +0000

    Initial template for website

---
+++ README.md
@@ -0,0 +1,4 @@
+# theamoebawebsite
+This is the repository which will hold code for the Amoeba website.
+
+Developers discussion on theamoeba.slack.com
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 8080;
+var http = require('http').Server(app);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function(){
+  console.log('Express server listening on port ' + port);
+});
