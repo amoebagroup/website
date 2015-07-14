4d65fd7178af6d1c76b5247fa2a92a6c22655f73
Rachna Singhal <rachna3singhal@gmail.com>
2015-07-14 15:52:48 +0530

    join us

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 'use strict'; 
-var express = require('express');
-
-var app = express();
-
-var port = process.env.PORT || 8080;
-var http = require('http').Server(app);
+var express = require('express'),
+	http 	= require('http'),
+	routes 	= require('./routes'),
+	app 	= express(),
+	port 	= process.env.PORT || 8080,
+	server 	= http.createServer(app),
+	bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use('/', routes);
 
-http.listen(port, function(){
+server.listen(port, function(){
   console.log('Express server listening on port ' + port);
 });
